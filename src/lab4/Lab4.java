c33d5116f2071c2d12f1a5b11eda7bc4dc4ea039
lixvin1 <hussein.imadian@gmail.com>
2025-10-15 01:07:07 +0300

    test with ayman

--- src/lab4/Lab4.java
+++ src/lab4/Lab4.java
@@ -10,11 +10,15 @@ package lab4;
  */
 public class Lab4 {
 
-    /**
+    /** 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        System.out.println("hi");
     }
     
 }
