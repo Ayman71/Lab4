9816c19abe45fe134f64c933b559a2fcaa0b53b4
lixvin1 <hussein.imadian@gmail.com>
2025-10-16 01:59:55 +0300

    2nd class done

--- src/lab4/AdminRole.java
+++ src/lab4/AdminRole.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab4;
 
 /**
@@ -9,5 +5,57 @@ package lab4;
  * @author husse
  */
 public class AdminRole {
+
+    private EmployeeUserDatabase database;
+
+    public AdminRole() {   // compostion relationship as admin role is part of data base
+        database = new EmployeeUserDatabase("Employees.txt");
+
+        database.readFromFile();
+
+    }
+
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) throws FileNotFoundException {
+
+        // created new emp to send it when inserting it inside the insertrecord and also this is assosication process
+        EmployeeUser newEmp = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+
+// duplicate employee check
+        if (database.contains(employeeId)) {
+            System.out.println("cant add this employee already exists");
+            return;
+        } else {
+            database.insertRecord(newEmp);
+
+        }
+
+    }
+
+    public EmployeeUser[] getListOfEmployees() {
+
+        // had to conert it since the requirment is to return array not array list and this method returns array list 
+        return database.returnAllRecords().toArray(new EmployeeUser[0]);
+
+    }
+
+    public void removeEmployee(String key) throws FileNotFoundException {
+        if (!(database.contains(key))
+                   {
+            System.out.println("cant delete this employee not exsiting");
+            return;
+
+        } else {
+
+            database.deleteRecord(key);
+
+        }
     
+    
+
+    public void logout() throws FileNotFoundException {
+
+        database.saveToFile();
+    }
+}
+
 }
